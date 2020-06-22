3ea1abaccbf3f8026cdd8c39c9857123ca6f7502
Moriah Humphries <moriah.humphries@gmail.com>
2020-06-22 10:26:07 -0500

    updated file paths

--- src/main/java/com/codeup/spring/controllers/MathController.java
+++ src/main/java/com/codeup/spring/controllers/MathController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 ///add/3/and/4	7
 @Controller
 public class MathController {
-    @RequestMapping(path = "add/{number1}/and/{number2}", method = RequestMethod.GET)
+    @RequestMapping(path = "/add/{number1}/and/{number2}", method = RequestMethod.GET)
     @ResponseBody
     public String add(@PathVariable int number1, @PathVariable int number2 ) {
         return 3 + " plus " + 4 + " is equal to " + (number1 + number2) + "!";
@@ -17,7 +17,7 @@ public class MathController {
 
 //    /subtract/3/from/10	7
 
-    @RequestMapping(path = "subtract/{number1}/from/{number2}", method = RequestMethod.GET)
+    @RequestMapping(path = "/subtract/{number1}/from/{number2}", method = RequestMethod.GET)
     @ResponseBody
     public String subtract(@PathVariable int number1, @PathVariable int number2 ) {
         return 10 + " minus " + 3 + " is equal to " + (number2 - number1) + "!";
@@ -26,13 +26,13 @@ public class MathController {
 
 //            /multiply/4/and/5	20
 
-    @RequestMapping(path = "multiply/{number1}/and/{number2}", method = RequestMethod.GET)
+    @RequestMapping(path = "/multiply/{number1}/and/{number2}", method = RequestMethod.GET)
     @ResponseBody
     public String multiply(@PathVariable int number1, @PathVariable int number2 ) {
         return 4 + " times " + 5 + " is equal to " + (number1 * number2) + "!";
     }
 //            /divide/6/by/3	2
-    @RequestMapping(path = "divide/{number1}/by/{number2}", method = RequestMethod.GET)
+    @RequestMapping(path = "/divide/{number1}/by/{number2}", method = RequestMethod.GET)
     @ResponseBody
     public String divide(@PathVariable int number1, @PathVariable int number2 ) {
         return 6 + " divided by " + 3 + " is equal to " + (number1 / number2) + "!";
