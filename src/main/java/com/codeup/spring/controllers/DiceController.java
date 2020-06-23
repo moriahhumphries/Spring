4f65d218ceeb5dd1b0037b5a593bc6895657e086
Moriah Humphries <moriah.humphries@gmail.com>
2020-06-23 09:55:40 -0500

    setting up question 2 and 3 project structure

---
+++ src/main/java/com/codeup/spring/controllers/DiceController.java
@@ -0,0 +1,32 @@
+package com.codeup.spring.controllers;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class DiceController {
+    @GetMapping("/roll-dice")
+    public String rollDice(){
+        return "roll-dice-game/roll-dice";
+    }
+
+    @GetMapping("/roll-dice/{guess}")
+    public String rollDiceGuess(@PathVariable int guess, Model model){
+        String message;
+
+        int random = (int) Math.ceil(Math.random() * 6);
+
+        if (random == guess) {
+            message = "You guessed correct!";
+        } else {
+            message = "You guessed wrong.";
+        }
+
+        model.addAttribute("message", message);
+        model.addAttribute("guess", guess);
+        model.addAttribute("random", random);
+
+        return "roll-dice-game/roll-dice";
+    }
+}
