cf6cf82b5fdff991638f42c202fc5d1da36bee37
Moriah Humphries <moriah.humphries@gmail.com>
2020-06-30 11:58:02 -0500

    updated posts files

--- src/main/java/com/codeup/spring/daos/PostsRepository.java
+++ src/main/java/com/codeup/spring/daos/PostsRepository.java
@@ -1,6 +1,16 @@
 package com.codeup.spring.daos;
 import com.codeup.spring.models.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface PostsRepository extends JpaRepository<Post, Long>  {
+import java.util.List;
+
+public interface PostsRepository extends JpaRepository<Post, Long> {
+    // HQL
+    @Query("from Post where title like %:term% or body like %:term%")
+    List<Post> searchByTitle(@Param("term") String term);
+
+    // query methods
+    Post findFirstByTitle(String title); // select * from ads where title = ? limit 1
 }
--- src/main/java/com/codeup/spring/daos/UsersRepository.java
+++ src/main/java/com/codeup/spring/daos/UsersRepository.java
@@ -2,5 +2,5 @@ package com.codeup.spring.daos;
 import com.codeup.spring.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UsersRepository extends JpaRepository<User, Long> {
 }
