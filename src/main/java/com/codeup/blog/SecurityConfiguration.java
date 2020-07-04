27ce5cb16cd11a8f4a98df4e3bfe3f34830ffe47
Moriah Humphries <moriah.humphries@gmail.com>
2020-07-04 16:24:44 -0500

    removed security config update for css

--- src/main/java/com/codeup/blog/SecurityConfiguration.java
+++ src/main/java/com/codeup/blog/SecurityConfiguration.java
@@ -32,13 +32,13 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         ;
     }
 
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        //Web resources
-        web.ignoring().antMatchers("/css/**");
-        web.ignoring().antMatchers("/scripts/**");
-        web.ignoring().antMatchers("/img/**");
-    }
+//    @Override
+//    public void configure(WebSecurity web) throws Exception {
+//        //Web resources
+//        web.ignoring().antMatchers("/css/**");
+//        web.ignoring().antMatchers("/scripts/**");
+//        web.ignoring().antMatchers("/img/**");
+//    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
